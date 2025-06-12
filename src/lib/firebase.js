975487f2a5ffe24b5c3e7117f14cf8916402eb92
Manuel <162144582+ManuelHidalgo26@users.noreply.github.com>
2025-06-12 18:53:00 -0300

    Fetch products from Firebase

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,6 @@
-
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-
-
+import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAWZQqqFG3CDm3Qi27S8nVM6lBlpZxc8LY",
@@ -12,9 +9,16 @@ const firebaseConfig = {
   storageBucket: "galactic-worldd.firebasestorage.app",
   messagingSenderId: "235508839573",
   appId: "1:235508839573:web:7c47eae58bb25e9858979c",
-  measurementId: "G-8C0ZBY3GH2"
+  measurementId: "G-8C0ZBY3GH2",
 };
-
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+
+export const getProductos = async () => {
+  const snap = await getDocs(collection(db, "productos"));
+  return snap.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+};
+
+export { app, analytics };
